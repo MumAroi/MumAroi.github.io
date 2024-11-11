eaa69ae75991a32cd67a8f337aed08abb7f40a41
LookNow <paramas.wae.th@gmail.com>
2024-11-11 17:23:58 +0700

    🚀 deploy: new version

---
+++ astro.config.mjs
@@ -0,0 +1,51 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
+import remarkUnwrapImages from 'remark-unwrap-images'
+import rehypeExternalLinks from 'rehype-external-links'
+import expressiveCode from 'astro-expressive-code'
+import { expressiveCodeOptions } from './src/site.config'
+import icon from 'astro-icon'
+
+const SERVER_PORT = 3000
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`
+const LIVE_URL = 'https://mumaroi.github.io'
+
+const SCRIPT = process.env.npm_lifecycle_script || ""
+const isBuild = SCRIPT.includes("astro build")
+const BASE_URL = isBuild ? LOCALHOST_URL : LIVE_URL
+
+// https://astro.build/config
+export default defineConfig({
+	server: { port: SERVER_PORT },
+	site: BASE_URL,
+	integrations: [
+		expressiveCode(expressiveCodeOptions),
+		tailwind({
+			applyBaseStyles: false
+		}),
+		sitemap(),
+		mdx(),
+		icon()
+	],
+	markdown: {
+		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					target: '_blank',
+					rel: ['nofollow, noopener, noreferrer']
+				}
+			]
+		],
+		remarkRehype: {
+			footnoteLabelProperties: {
+				className: ['']
+			}
+		}
+	},
+	prefetch: true,
+})
