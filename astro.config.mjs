8b655f9afb551c249a7eb8b2afbe6dc2f170cc14
LookNow <paramas.wae.th@gmail.com>
2024-11-11 17:32:33 +0700

    ♻️ refactor: set base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,11 @@ const LIVE_URL = 'https://mumaroi.github.io'
 
 const SCRIPT = process.env.npm_lifecycle_script || ""
 const isBuild = SCRIPT.includes("astro build")
-const BASE_URL = isBuild ? LOCALHOST_URL : LIVE_URL
+let BASE_URL = LOCALHOST_URL
+
+if (isBuild) {
+	BASE_URL = LIVE_URL
+}
 
 // https://astro.build/config
 export default defineConfig({
